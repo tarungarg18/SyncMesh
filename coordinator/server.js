9f03df4953ebc0265c5b720cf4f6fed7a2cfffc9
Padma0009 <padmabrahma9@gmail.com>
2026-09-05 19:13:49 +0530

    node health check

--- coordinator/server.js
+++ coordinator/server.js
@@ -2,10 +2,17 @@ const express = require("express");
 
 const app = express();
 const port = 8000;
+const HEARTBEAT_TIMEOUT_MS = 10000;
 
 const nodes = {};
 
 app.use(express.json());
+
+function nodeWithStatus(node) {
+  const status =
+    Date.now() - node.lastSeen > HEARTBEAT_TIMEOUT_MS ? "OFFLINE" : "ONLINE";
+  return { ...node, status };
+}
 
 app.get("/", (req, res) => {
   res.send("SyncMesh Coordinator is running");
@@ -13,12 +20,21 @@ app.get("/", (req, res) => {
 
 app.post("/nodes/register", (req, res) => {
   const { nodeId, port: nodePort } = req.body;
-  nodes[nodeId] = { nodeId, port: nodePort };
-  res.json(nodes[nodeId]);
+  nodes[nodeId] = { nodeId, port: nodePort, lastSeen: Date.now() };
+  res.json(nodeWithStatus(nodes[nodeId]));
+});
+
+app.post("/nodes/heartbeat", (req, res) => {
+  const { nodeId } = req.body;
+  if (!nodes[nodeId]) {
+    return res.json({});
+  }
+  nodes[nodeId].lastSeen = Date.now();
+  res.json(nodeWithStatus(nodes[nodeId]));
 });
 
 app.get("/nodes", (req, res) => {
-  res.json(Object.values(nodes));
+  res.json(Object.values(nodes).map(nodeWithStatus));
 });
 
 app.listen(port, () => {
