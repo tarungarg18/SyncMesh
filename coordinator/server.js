74d5bce8149fb1a7cd9d0f76e7c99eb65f6a2f9d
Padma0009 <padmabrahma9@gmail.com>
2026-09-05 19:09:28 +0530

    node registration

--- coordinator/server.js
+++ coordinator/server.js
@@ -3,8 +3,22 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const nodes = {};
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("SyncMesh Coordinator is running");
+});
+
+app.post("/nodes/register", (req, res) => {
+  const { nodeId, port: nodePort } = req.body;
+  nodes[nodeId] = { nodeId, port: nodePort };
+  res.json(nodes[nodeId]);
+});
+
+app.get("/nodes", (req, res) => {
+  res.json(Object.values(nodes));
 });
 
 app.listen(port, () => {
